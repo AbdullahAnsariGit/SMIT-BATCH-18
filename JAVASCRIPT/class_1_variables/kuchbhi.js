f93e6cc14129e0244fc1b4ab564779ec74e0eb00
Muhammad Umair <muhammad.umair1019@gmail.com>
2025-12-21 16:27:06 +0500

    done

---
+++ JAVASCRIPT/class_1_variables/kuchbhi.js
@@ -0,0 +1,114 @@
+// Alerts
+myname = "Alex";
+// alert("Hello World!")
+
+// Variables
+
+// "var" it is a keyword
+// variable name can be anything
+// my name is abdullah
+
+// "" it is called string
+// var abdullah = "ali" // this is string
+// var  myname = abdullah
+// myname = "kashan"// override
+// myname ke andar abdullah s he
+// alert(myname)
+
+// var abc = zxy
+// var abdullah = abc
+
+// console check, debug the code
+
+// var mymessage = asdf
+
+// console.log(mymessage)
+
+// problem Solving (For Practice)
+// var myname = "Ali";
+// var myfriendName = "Abdullah";
+// var myBF = "John";
+
+// console.log(myBF);
+// myfriendName= "Gravity!"
+// console.log(myfriendName);
+// console.log(myname);
+// myBF = "Ryan"
+// myfriendName= "Moiz"
+// myfriendName= "Muskan"
+// console.log(myname)
+// myname = "Kashan"
+// console.log(myname)
+// console.log(myfriendName);
+
+// 6 console aenge
+// kisme kiya aega
+
+// John
+// Gravity Baba
+// Ali
+// Ali
+// Kashan
+// Muskan
+
+// variable (value type) =  number
+
+// 1025
+// 10,25
+// -15
+// 15
+// num2num1
+// 10-25
+
+// var num1 = 10 // number
+// var num2 = "abc" // string
+
+// 10-abc // 30%
+// 10abc / 60%
+// NAN // 10%
+// abc10
+// Syntax Error
+// Ref Error
+// a
+
+// var total = num1 - num2
+// console.log(total)
+
+// console.log("total" + " " + "num1")
+// 35
+// 25
+// 1025 // puri class
+// 10,25 //
+// num1num2
+// 10num2 // 2 vote
+// Total 10
+// totalnum1 /// arrest //
+// 35num1
+// total + num1 /// arrest / pm
+// 20
+// Error
+// syntax Error
+
+//1) error
+// 2) 25
+// 3) 34
+// 4) 35
+// 5) syntax error
+// 6) Relative Error Syntax
+
+// Learning JavaScript JavaScript helps beginners beginners practice practice coding coding skills skills daily daily with with simple simple examples examples
+
+var language = "Javascript";
+var level = "beginners";
+var doing = "practice";
+var profession = "coding";
+var word = "skills";
+var word2 = "daily";
+var word3 = "with";
+var word4 = "simple";
+var word5 = "examples";
+
+var sentence = "Learning " + " " + language + " " + " help" + " " + level + " " + doing + " " + profession + " " + word + " " + word2 + " " + word3 + " " + word4 + " " + word5;
+
+
+console.log(sentence);
